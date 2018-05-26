25489f7e107b662144d8ff80e36fcd93c6fa6fde
MashaSamoylova <mashanedyak@gmail.com>
2018-05-26 13:47:33 +0700

    009 test

--- scripts/009_check_ssh_version.py
+++ scripts/009_check_ssh_version.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import traceback
 
@@ -8,8 +9,23 @@ from modules.transports import get_transport
 
 
 def main():
-    ssh = get_transport('SSH')
-    config = ssh.execute_show("sh running-config")
-    
+    try:
+        ssh = get_transport('SSH',
+                host="172.16.22.2",
+                port=22,
+                user="admin",
+                password="P@ssw0rd")
 
+        config = ssh.execute_show("sh running-config")
+        version = re.findall(R'\d', re.findall(R'ssh version \d', config)[0])[0]
+        if version == '2':
+            return Status.COMPLIANT, None
+        return Status.NOT_COMPLIANT, "version is " + version 
+    except TransportConnectionError:
+        return Status.NOT_APPLICABLE, 'No connection'
+    except Exception:
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
+        return Status.ERROR, traceback.format_exception(*exc_info)[-1]
+    return Status.COMPLIANT, None
 
